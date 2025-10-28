2859cc5f748872ceb80379790a59909a9ef010e1
Tobi Adebowale <princetobi2005@gmail.com>
2025-10-28 17:54:34 -0600

    code implements deleteCity()

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,8 +18,13 @@ public class CustomList {
     public boolean hasCity(City city){
         return cities.contains(city);
     }
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
 
-    
+
+
+
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
